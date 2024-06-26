d0b778dbed92d0f0f84bae08d1863e626eeb7558
Tobasco99 <kiel.tobi99@googlemail.com>
2024-06-26 11:46:05 +0200

    context closed

--- app/src/main/java/i5/las2peer/services/SurveyHandler/Participant.java
+++ app/src/main/java/i5/las2peer/services/SurveyHandler/Participant.java
@@ -389,7 +389,6 @@ public class Participant {
                     if(!answerOptions.isEmpty())
                         response.put("interactiveElements", answerOptions);
                     response.put("channel", channel);
-                    response.put("closeContext", false);
                 } else {
                     response.put("text", messageText);
                 }
@@ -442,7 +441,6 @@ public class Participant {
             if(!answerOptions.isEmpty())
                 response.put("interactiveElements", answerOptions);
             response.put("channel", channel);
-            response.put("closeContext", false);
         } else {
             response.put("text", skipText + messageText);
         }
